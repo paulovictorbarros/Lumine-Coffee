6df5a9919f7d77901fb68f925dc723886ec534b4
Paulo Victor <paulovictorbarrosdecarvalho@gmail.com>
2023-08-19 14:58:32 -0300

    Adicionei o bottom navigation view a algumas cores para o app

--- app/src/main/java/com/pvbcsoft/luminecoffee/MainActivity.kt
+++ app/src/main/java/com/pvbcsoft/luminecoffee/MainActivity.kt
@@ -2,10 +2,27 @@ package com.pvbcsoft.luminecoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.pvbcsoft.luminecoffee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
